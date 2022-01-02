a07123a8da34a7fd58cc660693d08ab298012cd0
Joe L'Salas <joluui22@gmail.com>
2022-01-02 14:40:43 -0300

    first commit

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPfxeE6wBMNtJce-ZdY8bevdVuAgoXKaE",
-  authDomain: "instagram-a9adb.firebaseapp.com",
-  projectId: "instagram-a9adb",
-  storageBucket: "instagram-a9adb.appspot.com",
-  messagingSenderId: "619809791888",
-  appId: "1:619809791888:web:8f7c7ea794def7055fdd15"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
